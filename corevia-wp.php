ab2228377a7e0a4f72b43c90e5e51bbd68685e13
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-04-04 14:29:11 +0600

    Update: Plugin header

--- corevia-wp.php
+++ corevia-wp.php
@@ -11,16 +11,6 @@
  * Requires PHP: 7.4
  * Text Domain: corevia-wp
  * Domain Path: /languages
- *
- * Corevia WP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * Corevia WP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
  */
 
 namespace CoreviaWP;
